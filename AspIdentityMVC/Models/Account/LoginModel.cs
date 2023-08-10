a697c75edfd240bdb9779a5b44561f0aaceda1a1
Mh. Saleem Al-Zayat <mselim@toptech.com.eg>
2023-08-11 02:51:08 +0300

    Add [Authorize] attribute for Home and Account controller. Add Login action to Account controller. Add login view with a basic login design. Project now must route from Index page in Home controoler to Login page in Account controller.

---
+++ AspIdentityMVC/Models/Account/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspIdentityMVC.Models.Account;
+
+public class LoginModel
+{
+    [Required(ErrorMessage = "The Email field is required.")]
+    public string Username { get; set; }
+    [Required(ErrorMessage ="Password required.")]
+    public string Password { get; set; }
+    public bool RememberLogin { get; set; }
+    public string ReturnUrl { get; set; }
+}
